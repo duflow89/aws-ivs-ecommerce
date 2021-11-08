c8cdb7aa58b4c3ea54fac37e970f88fbaf5d8784
duflow89 <duflow89@gmail.com>
2021-11-09 01:39:32 +0900

    feat: fix tsconfig for lint

---
+++ src/@types/global.d.ts
@@ -0,0 +1,13 @@
+import IVSPlayer from 'amazon-ivs-player';
+
+declare global {
+  interface Window {
+    IVSPlayer;
+  }
+}
+
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.png';
+declare module '*.svg';
+declare module '*.svg';
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,12 @@
 const App = () => {
-  const { IVSPlayer } = window;
-  console.log(IVSPlayer.isPlayerSupported);
+  const { isPlayerSupported } = window.IVSPlayer;
+  console.log(isPlayerSupported);
 
   return (
     <>
       <div>
-        <h1>Hello World</h1>
+        <h1>AWS IVS E-COMMERCE</h1>
+        <h2>isPlayerSupported: {isPlayerSupported.toString()}</h2>
       </div>
     </>
   );
